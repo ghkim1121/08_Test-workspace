bb7efc8f9d270ca566900e969e4076de0021e67b
anwlsangel <anwlsangel@gmail.com>
2024-05-28 17:00:27 +0900

    로그인, 로그아웃 기능 추가 - 무진
    MemberController 클래스 안에 작성하였음

--- Final_Project/src/main/java/com/kh/spring/member/controller/MemberController.java
+++ Final_Project/src/main/java/com/kh/spring/member/controller/MemberController.java
@@ -1,5 +1,31 @@
 package com.kh.spring.member.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.spring.member.model.vo.Member;
+
+@Controller
 public class MemberController {
+	
+	// 무진 - 240528
+	// 로그인 메소드
+	@RequestMapping("login.me")
+	public String loginMember(Member m) {
+		
+		return "redirect:/";
+	}
+
+	// 무진 - 240528
+	// 로그아웃 메소드
+	@RequestMapping("logout.me")
+	public String logoutMember(HttpSession session) {
+		
+		session.invalidate();
+		
+		return "redirect:/";
+	}
 
 }
